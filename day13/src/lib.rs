0863b58f64a76849d6d97e78a4535cc5f2029b32
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2023-01-05 19:30:43 -0600

    go back to old approach with trees
    	modified:   day13/Cargo.toml
    	new file:   day13/fulllog
    	new file:   day13/log
    	new file:   day13/mylog
    	modified:   day13/src/lib.rs
    	modified:   day13/src/packet.rs
    	new file:   day13/test2.txt
    	new file:   day13/testlog
    	new file:   day13/tmp

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -1,148 +1,83 @@
 use fs_err as fs;
+use id_tree::*;
+use itertools::EitherOrBoth::{Both, Left, Right};
+use itertools::Itertools;
 use std::error::Error;
 
 mod packet;
+use crate::packet::parse::{compare_vals, get_value, parse_tree};
 use crate::packet::parse::{Correct, Incorrect, Order};
-use crate::packet::{LEFTBRACE, RIGHTBRACE};
 
-pub fn test(left_packet: &str, right_packet: &str) -> Order {
-    let mut advance_left = true;
-    let mut advance_right = true;
-    let mut left_elem: Option<char> = None;
-    let mut right_elem: Option<char> = None;
+pub fn test(mut left_tree: Tree<i32>, mut right_tree: Tree<i32>) -> Order {
+    let left_root = left_tree
+        .root_node_id()
+        .expect("left packet tree has no root!");
+    let right_root = right_tree
+        .root_node_id()
+        .expect("right packet tree has no root!");
 
-    let mut left: Option<i32> = None;
-    let mut right: Option<i32> = None;
-    let mut left_depth: i32 = 0;
-    let mut right_depth: i32 = 0;
+    let mut left_children: Vec<NodeId> = Vec::new();
+    let mut right_children: Vec<NodeId> = Vec::new();
+    for lchild in left_tree.children_ids(&left_root).unwrap() {
+        left_children.push(lchild.clone());
+    }
+    for rchild in right_tree.children_ids(&right_root).unwrap() {
+        right_children.push(rchild.clone());
+    }
 
-    let mut transition_left = false;
-    let mut transition_right = false;
+    let it = left_children
+        .into_iter()
+        .zip_longest(right_children.into_iter());
 
-    let mut left_iter = left_packet.chars().peekable();
-    let mut right_iter = right_packet.chars().peekable();
-
-    loop {
-        if advance_left {
-            left_elem = left_iter.next();
-        }
-        if advance_right {
-            right_elem = right_iter.next();
-        }
-
-        match (left_elem, right_elem) {
-            (Some(left_val), Some(right_val)) => {
-                // try to see if they are numbers
-                if left_val.is_digit(10) {
-                    let mut left_str = String::new();
-                    left_str.push(left_val);
-                    if left_iter.peek().is_some() {
-                        if left_iter.peek().unwrap().is_digit(10) {
-                            left_str.push(*left_iter.peek().unwrap());
-                        }
-                    }
-                    left = Some(left_str.parse::<i32>().expect("non-numeric input!"));
-                }
-                if right_val.is_digit(10) {
-                    let mut right_str = String::new();
-                    right_str.push(right_val);
-                    if right_iter.peek().is_some() {
-                        if right_iter.peek().unwrap().is_digit(10) {
-                            right_str.push(*right_iter.peek().unwrap());
-                        }
-                    }
-                    right = Some(right_str.parse::<i32>().expect("non-numeric input!"));
-                }
+    for elem in it {
+        match elem {
+            Both(left_child, right_child) => loop {
+                let left = get_value(&mut left_tree, &left_child);
+                let right = get_value(&mut right_tree, &right_child);
+                let empty_left = left_tree.get(&left_child).is_err();
+                let empty_right = right_tree.get(&right_child).is_err();
                 match (left, right) {
-                    // both are numbers, compare them
-                    (Some(lnum), Some(rnum)) => {
-                        println!("comapring number pair {}/{}", lnum, rnum);
-
-                        if lnum < rnum {
-                            return Order::Correct(Correct::LeftSideIsSmaller);
-                        }
-                        if rnum < lnum {
-                            return Order::Incorrect(Incorrect::RightSideIsSmaller);
-                        }
-                        if lnum == rnum {
-                            left = None;
-                            right = None;
-                            advance_left = true;
-                            advance_right = true;
+                    (Some(left_val), Some(right_val)) => {
+                        println!("pair of vals extracted are : {}/{}", left_val, right_val);
+                        let result = compare_vals(left_val, right_val);
+                        if result.is_some() {
+                            return result.unwrap();
                         }
                     }
                     (Some(_), None) => {
-                        advance_left = false;
-                        advance_right = true;
-                        if transition_right {
-                            return Order::Incorrect(Incorrect::RightSideRanOutItems);
-                        }
+                        println!("number only on left");
+                        return Order::Incorrect(Incorrect::RightSideRanOutItems);
                     }
                     (None, Some(_)) => {
-                        advance_left = true;
-                        advance_right = false;
-                        if transition_left {
-                            return Order::Correct(Correct::LeftSideRanOutItems);
-                        }
+                        println!("number only on right");
+                        return Order::Correct(Correct::LeftSideRanOutItems);
                     }
-                    _ => {}
+                    (None, None) => {
+                        println!("no number on both sides");
+                    }
                 }
-
-                if left_val == LEFTBRACE {
-                    left_depth += 1;
-                    transition_left = false;
+                match (empty_left, empty_right) {
+                    (false, true) => {
+                        return Order::Incorrect(Incorrect::RightSideRanOutItems);
+                    }
+                    (true, false) => {
+                        return Order::Correct(Correct::LeftSideRanOutItems);
+                    }
+                    (false, false) => {
+                        continue;
+                    }
+                    (true, true) => {
+                        break;
+                    }
                 }
-                if left_val == RIGHTBRACE {
-                    left_depth -= 1;
-                    transition_left = true;
-                }
-                if right_val == LEFTBRACE {
-                    right_depth += 1;
-                    transition_right = false;
-                }
-                if right_val == RIGHTBRACE {
-                    right_depth -= 1;
-                    transition_right = true;
-                }
-
-                println!(
-                    "reading character pair at is {}/{}; at depth difference {}; transition status {}/{}",
-                    left_val, right_val, (left_depth - right_depth), transition_left, transition_right
-                );
-
-                if left_iter.peek().is_some()
-                    && *left_iter.peek().unwrap() == ','
-                    && (left_depth - right_depth).abs() > 1
-                {
-                    return Order::Correct(Correct::LeftSideRanOutItems);
-                }
-                if right_iter.peek().is_some()
-                    && *right_iter.peek().unwrap() == ','
-                    && (left_depth - right_depth).abs() > 1
-                {
-                    return Order::Incorrect(Incorrect::RightSideRanOutItems);
-                }
-
-                if transition_left && !transition_right {
-                    return Order::Correct(Correct::LeftSideRanOutItems);
-                }
-                if !transition_left && transition_right {
-                    return Order::Incorrect(Incorrect::RightSideRanOutItems);
-                }
-
-                continue;
-            }
-
-            (Some(_), None) => {
+            },
+            Left(_) => {
                 return Order::Incorrect(Incorrect::RightSideRanOutItems);
             }
-            (None, Some(_)) => {
+            Right(_) => {
                 return Order::Correct(Correct::LeftSideRanOutItems);
             }
-            _ => {}
         }
-
-        break;
     }
 
     return Order::Correct(Correct::LeftSideIsSmaller);
@@ -165,22 +100,24 @@ pub fn run(filename: String) -> Result<(), Box<dyn Error>> {
 
             let left_packet: String = chunk.get(0).expect("no packet on left!").to_string();
             let right_packet: String = chunk.get(1).expect("no packet on right!").to_string();
-            /*
-            let check = trim_test(&mut left_packet, &mut right_packet);
-            if check.is_some() {
-                // check for early conclusion
-                println!(
-                    "pair-id: {}; conclusion-reached: {:?}",
-                    pair_id,
-                    check.unwrap()
-                );
-                return None;
-            }*/
 
-            println!("left packet is {}", left_packet);
-            println!("right packet is {}", right_packet);
+            let left_tree = parse_tree(&left_packet);
+            let right_tree = parse_tree(&right_packet);
 
-            let result = test(&left_packet, &right_packet);
+            let mut leftstr = String::new();
+            left_tree
+                .clone()
+                .write_formatted(&mut leftstr)
+                .expect("cannot format tree");
+            println!("left tree is \n{}", leftstr);
+            let mut rightstr = String::new();
+            right_tree
+                .clone()
+                .write_formatted(&mut rightstr)
+                .expect("cannot format tree");
+            println!("right tree is \n{}", rightstr);
+
+            let result = test(left_tree, right_tree);
             println!("result is {:?}", result);
 
             match result {
--- day13/src/packet.rs
+++ day13/src/packet.rs
@@ -1,7 +1,14 @@
-pub const LEFTBRACE: char = '[';
-pub const RIGHTBRACE: char = ']';
+const LEFTBRACE: char = '[';
+const RIGHTBRACE: char = ']';
 
 pub mod parse {
+
+    use crate::packet::{LEFTBRACE, RIGHTBRACE};
+    use id_tree::InsertBehavior::*;
+    use id_tree::RemoveBehavior::DropChildren;
+    use id_tree::*;
+    use itertools::EitherOrBoth::{Both, Left, Right};
+    use itertools::Itertools;
 
     #[derive(PartialEq, Copy, Clone, Debug)]
     pub enum Correct {
@@ -19,5 +26,214 @@ pub mod parse {
     pub enum Order {
         Correct(Correct),
         Incorrect(Incorrect),
+    }
+
+    pub fn compare_vals(left_val: i32, right_val: i32) -> Option<Order> {
+        match left_val.cmp(&right_val) {
+            std::cmp::Ordering::Less => {
+                return Some(Order::Correct(Correct::LeftSideIsSmaller));
+            }
+            std::cmp::Ordering::Greater => {
+                return Some(Order::Incorrect(Incorrect::RightSideIsSmaller));
+            }
+            std::cmp::Ordering::Equal => {
+                return None;
+            }
+        }
+    }
+
+    pub fn compare(
+        ltree: &mut Tree<i32>,
+        lid: &NodeId,
+        rtree: &mut Tree<i32>,
+        rid: &NodeId,
+    ) -> Option<Order> {
+        let mut lcurr = lid.clone();
+        let mut rcurr = rid.clone();
+
+        let mut early_left: bool = false;
+        let mut early_right: bool = false;
+
+        let mut advance_left: bool = true;
+        let mut advance_right: bool = true;
+
+        loop {
+            let lcurr_width = ltree.children_ids(&lcurr).unwrap().count();
+            let rcurr_width = rtree.children_ids(&rcurr).unwrap().count();
+
+            match (lcurr_width, rcurr_width) {
+                (1, 1..) => {
+                    early_left = true;
+                }
+                (1.., 1) => {
+                    early_right = true;
+                }
+                (_, _) => {}
+            }
+
+            let mut lchildren_ids = ltree.children_ids(&lcurr).unwrap();
+            let mut rchildren_ids = rtree.children_ids(&rcurr).unwrap();
+
+            if advance_left {
+                match lchildren_ids.next() {
+                    Some(child) => {
+                        lcurr = child.clone();
+                    }
+                    None => {
+                        advance_left = false;
+                    }
+                }
+            }
+
+            if advance_right {
+                match rchildren_ids.next() {
+                    Some(child) => {
+                        rcurr = child.clone();
+                    }
+                    None => {
+                        advance_right = false;
+                    }
+                }
+            }
+            if !advance_left && !advance_right {
+                break;
+            }
+        }
+
+        let lval = ltree.get(&lcurr).unwrap().data().clone();
+        let rval = rtree.get(&rcurr).unwrap().data().clone();
+
+        _ = ltree.remove_node(lcurr, DropChildren).unwrap();
+        _ = rtree.remove_node(rcurr, DropChildren).unwrap();
+
+        if lval == rval {
+            if early_left {
+                return Some(Order::Correct(Correct::LeftSideRanOutItems));
+            }
+            if early_right {
+                return Some(Order::Incorrect(Incorrect::RightSideRanOutItems));
+            }
+        }
+
+        if lval == -1 && rval != -1 {
+            return Some(Order::Correct(Correct::LeftSideRanOutItems));
+        }
+
+        if lval != -1 && rval == -1 {
+            return Some(Order::Incorrect(Incorrect::RightSideRanOutItems));
+        }
+
+        return compare_vals(lval, rval);
+    }
+
+    pub fn get_value(tree: &mut Tree<i32>, id: &NodeId) -> Option<i32> {
+        let mut curr = id.clone();
+        let mut width: Vec<usize> = Vec::new();
+
+        loop {
+            let curr_width = tree.children_ids(&curr).unwrap().count();
+            let mut children_ids = tree.children_ids(&curr).unwrap();
+            match children_ids.next() {
+                Some(child) => {
+                    width.push(curr_width);
+                    curr = child.clone();
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+
+        let mut retval: Option<i32> = None;
+        let val = tree.get(&curr).unwrap().data();
+        if *val != -1 {
+            retval = Some(*val);
+        }
+        println!("elem {}, widths are", *val);
+        for w in width {
+            print!("{}, ", w);
+        }
+        println!("");
+
+        _ = tree.remove_node(curr, DropChildren).unwrap();
+
+        retval
+    }
+
+    pub fn parse_tree(line: &str) -> Tree<i32> {
+        let mut tree: Tree<i32> = TreeBuilder::new().with_node_capacity(5).build();
+        // let -2 represent the root node
+        //let root_id: NodeId = tree.insert(Node::new(-2), AsRoot).unwrap();
+        let mut curr: Option<NodeId> = None;
+
+        let mut input = line.chars();
+        let mut val_str = String::new();
+        let mut outer_brace = true;
+        while let Some(in_char) = input.next() {
+            //println!("in_char is : {}", in_char);
+            match in_char {
+                LEFTBRACE => {
+                    // let -1 represent a [] layer
+                    if outer_brace {
+                        curr = Some(tree.insert(Node::new(-1), AsRoot).unwrap());
+                        outer_brace = false;
+                        continue;
+                    } else {
+                        curr = Some(
+                            tree.insert(
+                                Node::new(-1),
+                                InsertBehavior::UnderNode(&curr.as_ref().unwrap()),
+                            )
+                            .unwrap(),
+                        );
+                        continue;
+                    }
+                }
+                RIGHTBRACE => {
+                    if let Ok(elem) = val_str.trim().parse::<i32>() {
+                        // there will always be a parentval for a well formed
+                        // packet!
+                        tree.insert(
+                            Node::new(elem),
+                            InsertBehavior::UnderNode(&curr.as_ref().unwrap()),
+                        )
+                        .unwrap();
+                        val_str.clear();
+                    }
+
+                    if tree
+                        .get(&curr.as_ref().unwrap())
+                        .unwrap()
+                        .parent()
+                        .is_some()
+                    {
+                        curr = Some(
+                            tree.get(&curr.as_ref().unwrap())
+                                .unwrap()
+                                .parent()
+                                .unwrap()
+                                .clone(),
+                        );
+                    }
+                    continue;
+                }
+                ',' => {
+                    if let Ok(elem) = val_str.trim().parse::<i32>() {
+                        tree.insert(
+                            Node::new(elem),
+                            InsertBehavior::UnderNode(&curr.as_ref().unwrap()),
+                        )
+                        .unwrap();
+                        val_str.clear();
+                    }
+                    continue;
+                }
+                _ => {
+                    val_str.push(in_char);
+                }
+            }
+        }
+
+        tree
     }
 }
